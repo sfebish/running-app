e1c819434c9fb04b0349c414afde729fafe78a0c
sfebish <32653326+sfebish@users.noreply.github.com>
2018-03-07 13:47:49 +0000

    Update databaseConnection.js

--- RunApplication/server/databaseConnection.js
+++ RunApplication/server/databaseConnection.js
@@ -3,7 +3,13 @@
 // Use the native ES6 promise instead of mongoose call backs
 // This makes the code more readable
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/rundb');
+
+const opts = {
+  useMongoClient: true,
+  promiseLibrary: global.Promise;
+}
+
+mongoose.connect('mongodb://localhost/rundb', opts);
 
 var db = mongoose.connection;
 db.on('error', (error) => console.error(error));
