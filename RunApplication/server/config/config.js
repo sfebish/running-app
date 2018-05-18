e6d07712860b8040929a5026d217a5e2a6d15fd1
sfebish <sjfebish@gmail.com>
2018-05-18 14:47:02 -0400

    Add configuration management to server

---
+++ RunApplication/server/config/config.js
@@ -0,0 +1,24 @@
+﻿// This is the default config class
+// All production/ default values will be stored here
+// Local overrides should be stored in the config.overrides.js file
+// Note - override file will not be pushed to git, so you may need to create it
+export class Config {
+    constructor() {
+        this.databaseName = 'rundb';
+        this.databaseServer = 'database'; // database is the name of the docker compose service
+
+    }
+
+    getValue(paramName) {
+        return this[paramName];
+    }
+}
+
+export class ConfigTest extends Config {
+    constructor() {
+        super();
+
+        this.databaseName = 'rundb_test';
+        this.databaseServer = '127.0.0.1';
+    }
+}
